9711b5134fb8c0e023bbea87726a4a1ed87532af
Diego Guerra <dgoguerra.or@gmail.com>
2017-10-31 12:47:03 +0100

    removed dependencies, now parse everything manually
    Since parse-url's recent versions have changed and don't yield
    the same results anymore.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var trim = require('lodash/trim'),
-    parseUrl = require('parse-url');
 
 function stringifyConn(conn, defaults) {
     defaults = defaults || {};
@@ -45,63 +43,31 @@ module.exports.stringify = stringifyConn;
 function parseDbUri(uri, defaults) {
     defaults = defaults || {};
 
-    // if there was a user and password, they will have been
-    // extracted as a string separated by ':'
-    function parseUserPassword(userStr) {
-        var obj = {username: null, password: null};
+    var dbUriRegex = /^((\w+):\/\/)?(([^:]+)(:([^@]+))?@)?(([^\/:]+)(:(\d+))?)(\/(\w+)?)?$/;
 
-        var arr = userStr && userStr.split(':');
+    var match = uri.match(dbUriRegex);
 
-        if (!arr || arr.length === 0) {
-            return obj;
-        }
-
-        obj.username = decodeURIComponent(arr[0]);
-
-        if (arr.length === 2) {
-            obj.password = decodeURIComponent(arr[1]);
-        }
-
-        return obj;
-    }
-
-    var parsed = parseUrl(uri),
-        user = parseUserPassword(parsed.user);
-
-    // 'parse-url' sets the protocol to 'file' or 'ssh'
-    // if none was found and the uri looks like its one
-    // of those. don't let it do it
-    if (parsed.protocol === 'ssh' || parsed.protocol === 'file') {
-        parsed.protocol = '';
-    }
+    if (!match) return null;
 
     var conn = {
         uri: uri,
-        host: parsed.resource
+        protocol: match[2] || defaults.protocol,
+        // username or password's special characters may be encoded
+        username: match[4] && decodeURIComponent(match[4]) || defaults.username,
+        password: match[6] && decodeURIComponent(match[6]) || defaults.password,
+        host: match[8] || defaults.host,
+        port: Number(match[10]) || defaults.port,
+        database: match[12] || defaults.database
     };
 
-    if (parsed.protocol || defaults.protocol) {
-        conn.protocol = parsed.protocol || defaults.protocol;
-    }
-
-    if (parsed.port || defaults.port) {
-        conn.port = parsed.port || defaults.port;
-    }
-
-    var database = trim(parsed.pathname, '/');
-    if (database || defaults.database) {
-        conn.database = database || defaults.database;
-    }
-
-    if (user.username || defaults.username) {
-        conn.username = user.username || defaults.username;
-    }
-
-    if (user.password || defaults.password) {
-        conn.password = user.password || defaults.password;
-    }
-
     conn.fullUri = stringifyConn(conn);
+
+    // remove undefined keys from the result
+    Object.keys(conn).forEach(function(k) {
+        if (typeof conn[k] === 'undefined') {
+            delete conn[k];
+        }
+    });
 
     return conn;
 }
